5b88244c5e95a2b2e8a70a20ae23181df24167ae
Amaresh0612 <amaresh.dash0612@gmail.com>
2021-06-28 11:46:07 +0530

    final

--- generate_report.py
+++ generate_report.py
@@ -9,79 +9,81 @@ from daily_counts import plot_daily_count_states, plot_daily_count_countries
 from create_case_maps import plot_usa_case_map, plot_global_case_map
 from helper import Mode
 
+#For A4 size
 WIDTH = 210
 HEIGHT = 297
 
-TEST_DATE = "10/20/20"
+TEST_DATE = "10/20/20"#datetime.today  
 
 def create_title(day, pdf):
   # Unicode is not yet supported in the py3k version; use windows-1252 standard font
   pdf.set_font('Arial', '', 24)  
-  pdf.ln(60)
-  pdf.write(5, f"Covid Analytics Report")
+  pdf.ln(2)
+  pdf.write(5, f"Digi Analytics Report")
   pdf.ln(10)
   pdf.set_font('Arial', '', 16)
   pdf.write(4, f'{day}')
   pdf.ln(5)
 
-def create_analytics_report(day=TEST_DATE, filename="report.pdf"):
+def create_analytics_report(day=TEST_DATE, filename="DBS_Availability_report.pdf"):
   pdf = FPDF() # A4 (210 by 297 mm)
 
-  states = ['Massachusetts', 'New Hampshire']
+#  states = ['Massachusetts', 'New Hampshire']
 
   ''' First Page '''
   pdf.add_page()
-  pdf.image("./resources/letterhead_cropped.png", 0, 0, WIDTH)
+  pdf.image("./resources/DBS-Logo.png", x=150, y=2, w=50, h=20)
   create_title(day, pdf)
 
-  plot_usa_case_map("./tmp/usa_cases.png", day=day)
-  prev_days = 250
-  plot_states(states, days=prev_days, filename="./tmp/cases.png", end_date=day)
-  plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths.png", end_date=day)
+  # plot_usa_case_map("./tmp/usa_cases.png", day=day)
+  # prev_days = 250
+  # plot_states(states, days=prev_days, filename="./tmp/cases.png", end_date=day)
+  # plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths.png", end_date=day)
 
-  pdf.image("./tmp/usa_cases.png", 5, 90, WIDTH-20)
-  pdf.image("./tmp/cases.png", 5, 200, WIDTH/2-10)
-  pdf.image("./tmp/deaths.png", WIDTH/2, 200, WIDTH/2-10)
+  #pdf.image("./tmp/usa_cases.png", x=10, y=30, w=90, h=60)
+  pdf.image("./tmp/test.png", 5, 30, WIDTH/2-10) 
+  pdf.image("./tmp/stacked_bar.png", WIDTH/2, 30, WIDTH/2-10)
+  pdf.image("./tmp/usa_cases.png", x=5, y=120, w=190, h=90)
 
-  ''' Second Page '''
-  pdf.add_page()
+  # ''' Second Page '''
+  # pdf.add_page()
 
-  plot_daily_count_states(states, day=day, filename="./tmp/cases_day.png")
-  plot_daily_count_states(states, day=day, mode=Mode.DEATHS, filename="./tmp/deaths_day.png")
-  pdf.image("./tmp/cases_day.png", 5, 20, WIDTH/2-10)
-  pdf.image("./tmp/deaths_day.png", WIDTH/2, 20, WIDTH/2-10)
+  # plot_daily_count_states(states, day=day, filename="./tmp/cases_day.png")
+  # plot_daily_count_states(states, day=day, mode=Mode.DEATHS, filename="./tmp/deaths_day.png")
+  # pdf.image("./tmp/cases_day.png", 5, 20, WIDTH/2-10)d
+  # pdf.image("./tmp/deaths_day.png", WIDTH/2, 20, WIDTH/2-10)
 
-  prev_days = 7
-  plot_states(states, days=prev_days, filename="./tmp/cases2.png", end_date=day)
-  plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths2.png", end_date=day)
-  pdf.image("./tmp/cases2.png", 5, 110, WIDTH/2-10)
-  pdf.image("./tmp/deaths2.png", WIDTH/2, 110, WIDTH/2-10)
+  # prev_days = 7
+  # plot_states(states, days=prev_days, filename="./tmp/cases2.png", end_date=day)
+  # plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths2.png", end_date=day)
+  # pdf.image("./tmp/cases2.png", 5, 110, WIDTH/2-10)
+  # pdf.image("./tmp/deaths2.png", WIDTH/2, 110, WIDTH/2-10)
 
-  prev_days = 30
-  plot_states(states, days=prev_days, filename="./tmp/cases3.png", end_date=day)
-  plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths3.png", end_date=day)
-  pdf.image("./tmp/cases3.png", 5, 200, WIDTH/2-10)
-  pdf.image("./tmp/deaths3.png", WIDTH/2, 200, WIDTH/2-10)
+  # prev_days = 30
+  # plot_states(states, days=prev_days, filename="./tmp/cases3.png", end_date=day)
+  # plot_states(states, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths3.png", end_date=day)
+  # pdf.image("./tmp/cases3.png", 5, 200, WIDTH/2-10)
+  # pdf.image("./tmp/deaths3.png", WIDTH/2, 200, WIDTH/2-10)
 
-  ''' Third Page '''
-  pdf.add_page()
+  # ''' Third Page '''
+  # pdf.add_page()
 
-  plot_global_case_map("./tmp/global_cases.png", day=day)
+  # plot_global_case_map("./tmp/global_cases.png", day=day)
 
-  countries = ['US', 'India', 'Brazil']
-  prev_days = 7
-  plot_countries(countries, days=prev_days, filename="./tmp/cases4.png", end_date=day)
-  plot_countries(countries, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths4.png", end_date=day)
+  # countries = ['US', 'India', 'Brazil']
+  # prev_days = 7
+  # plot_countries(countries, days=prev_days, filename="./tmp/cases4.png", end_date=day)
+  # plot_countries(countries, days=prev_days, mode=Mode.DEATHS, filename="./tmp/deaths4.png", end_date=day)
 
-  pdf.image("./tmp/global_cases.png", 5, 20, WIDTH-20)
-  pdf.image("./tmp/cases4.png", 5, 130, WIDTH/2-10)
-  pdf.image("./tmp/deaths4.png", WIDTH/2, 130, WIDTH/2-10)
+  # pdf.image("./tmp/global_cases.png", 5, 20, WIDTH-20)
+  # pdf.image("./tmp/cases4.png", 5, 130, WIDTH/2-10)
+  # pdf.image("./tmp/deaths4.png", WIDTH/2, 130, WIDTH/2-10)
 
   pdf.output(filename, 'F')
 
 
 if __name__ == '__main__':
   yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y").replace("/0","/").lstrip("0")
-  yesterday = "10/10/20" # Uncomment line for testing
+  #yesterday = "10/10/20" # Uncomment line for testing
   
   create_analytics_report(yesterday)
